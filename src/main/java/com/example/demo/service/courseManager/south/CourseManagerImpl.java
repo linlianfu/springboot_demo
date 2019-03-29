3229784f581a06db2b217d618bc89d3775294146
linlianfu <1241352445@qq.com>
2019-03-29 22:59:18 +0800

    query查询增加参数查询

--- src/main/java/com/example/demo/service/courseManager/south/CourseManagerImpl.java
+++ src/main/java/com/example/demo/service/courseManager/south/CourseManagerImpl.java
@@ -5,6 +5,9 @@ import com.example.demo.model.CoursePO;
 import com.example.demo.service.courseManager.ICourseManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.mongodb.core.query.UntypedExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,6 +29,15 @@ public class CourseManagerImpl implements ICourseManager {
         List<CoursePO> all = courseDao.findAll();
 
         all.forEach(p->log.info(p.toString()));
+
+        CoursePO queryDto = new CoursePO();
+        queryDto.setName("测试排他锁111");
+
+        UntypedExampleMatcher untypedExampleMatcher = UntypedExampleMatcher.matchingAny()
+                .withIgnorePaths("id","period","abouts","delete","enable","iconPath")
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING).withIgnoreNullValues();
+        Example<CoursePO> example = Example.of(queryDto,untypedExampleMatcher);
+        List<CoursePO> conditionList = courseDao.findAll(example);
         return all;
     }
 }
